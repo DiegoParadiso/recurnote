d9e004a24dd9157df60862cb76c3658b3fb86e3b
Diego Gonzalez <diegoagonzalez33@gmail.com>
2025-10-19 04:10:51 -0300

    refactor: depuration-frontend

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -100,6 +100,7 @@ export default function Login() {
   };
 
   const handleGitHubToken = (event) => {
+    console.log('MSG EVENT', event); // Para debug
     if (!event.data || !event.data.token) return;
     try {
       localStorage.setItem('token', event.data.token);
